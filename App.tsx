80c4f6afea522819af5f33645dafb9c9f818b5d2
Felipe <flpc.dev@gmail.com>
2023-02-06 11:30:21 -0300

    app finished!

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from 'styled-components/native';
+
+import { Loading } from '@components/Loading';
+
+import theme from '@theme/index';
+import { StatusBar } from 'react-native';
+import { Routes } from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <View>
-      <Text>Teams</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      { fontsLoaded ? <Routes /> : <Loading /> }
+    </ThemeProvider>
+    
   );
 }
